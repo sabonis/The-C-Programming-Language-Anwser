aaa5e500d27241701ce34b1f59ba5859de8ce0b5
sabonis <sabonis.tw@gmail.com>
2013-11-11 23:12:21 +0800

    1-13 updated

--- exercises/1-13.c
+++ exercises/1-13.c
@@ -1,38 +1,45 @@
 #include<stdio.h>
 
-#define NUMBER_OF_CHAR 26
+#define IN      0  // inside a word
+#define OUT     1  // outsid a word
+#define MAXWORD 10 // max word lenth
 
 main()
 {
-    int c, i;
-    int charBase;
-    int nCount[NUMBER_OF_CHAR];
+	int c, i, state = OUT;
+	int wordCounts[MAXWORD]; // words array
+	int wordLenth;   		 // lenth of a word
+	int overflow = 0; 		 // number of overflow words
 
-    // initialization
-    charBase = 'a';
-    for (i = 0; i < NUMBER_OF_CHAR; i++) {
-        nCount[i] = 0;
-    }
+	/* initialization */
+	for(i = 0; i < MAXWORD; i++) {
+		wordCounts[i] = 0;
+	}
 
-    while((c = getchar()) != EOF) {
-        if(c >= 'A' && c <= 'Z') {
-            ++nCount[c-'A']; // uppercase character case
-        } else if(c >= 'a' && c <= 'z') {
-            ++nCount[c-'a']; // lowercase character case
-        }
-    }
+	while ((c = getchar()) != EOF) {
+		if (c == '\t' || c == '\n' || c == ' ') {
+			/* end of a worjkjd */
+			if (wordLenth > 0) {
+				if (wordLenth < MAXWORD) {
+					++wordCounts[wordLenth];
+				} else {
+					++overflow;
+				}
+			}
+			wordLenth = 0;
+			state = OUT;
+		} else if (state == OUT) {
+			/* beginning of a new word */
+			state = IN;					
+			wordLenth = 1;
+		} else {
+			/* inside a word */
+			++wordLenth;    
+		}
+	}
 
-    // header
-    putchar('\n');
-    for (i = 0; i < NUMBER_OF_CHAR; i++) {
-        printf("%3c", charBase+i);
-    }
-    // separator
-    printf("\n=================================================================================\n");
-    // char counts
-    putchar('\n');
-    for (i = 0; i < NUMBER_OF_CHAR; i++) {
-        printf("%3d", nCount[i]);
-    }
-    putchar('\n');
+	/* output */
+	for (i = 1; i < MAXWORD; i++) {
+		printf("%5d - %5d \n", i, wordCounts[i]);
+	}
 }

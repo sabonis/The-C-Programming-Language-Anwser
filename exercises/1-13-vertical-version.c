7f7364aa560183530fbc42c45d640ec6cb246e05
sabonis <sabonis.tw@gmail.com>
2013-11-12 08:28:47 +0800

    Update indentation

--- exercises/1-13-vertical-version.c
+++ exercises/1-13-vertical-version.c
@@ -5,55 +5,55 @@
 
 #include<stdio.h>
 
-#define IN      0       // inside a word
-#define OUT     1       // outsid a word
-#define MAXWORD 11      // max word lenth
+#define IN           0  // inside a word
+#define OUT          1  // outsid a word
+#define MAXWORD      11 // max word lenth
 #define MAXHISTOGRAM 15 // max histogram lenth
 
 main()
 {
-	int c, i, j, state = OUT;
-	int wordCounts[MAXWORD]; // words array
-	int wordLenth;           // lenth of a word
+    int c, i, j, state = OUT;
+    int wordCounts[MAXWORD]; // words array
+    int wordLenth;           // lenth of a word
     int len = 0;             // lenth of histogram
     int maxValue = 0;        // max value of words
-	int overflow = 0;        // number of overflow words
+    int overflow = 0;        // number of overflow words
 
-	/* initialization */
-	for(i = 0; i < MAXWORD; i++) {
-		wordCounts[i] = 0;
-	}
+    /* initialization */
+    for(i = 0; i < MAXWORD; i++) {
+        wordCounts[i] = 0;
+    }
 
-	while ((c = getchar()) != EOF) {
-		if (c == '\t' || c == '\n' || c == ' ') {
-			/* end of a worjkjd */
-			if (wordLenth > 0) {
-				if (wordLenth < MAXWORD) {
-					++wordCounts[wordLenth];
-				} else {
-					++overflow;
-				}
-			}
-			wordLenth = 0;
-			state = OUT;
-		} else if (state == OUT) {
-			/* beginning of a new word */
-			state = IN;					
-			wordLenth = 1;
-		} else {
-			/* inside a word */
-			++wordLenth;    
-		}
-	}
+    while ((c = getchar()) != EOF) {
+        if (c == '\t' || c == '\n' || c == ' ') {
+            /* end of a word */
+            if (wordLenth > 0) {
+                if (wordLenth < MAXWORD) {
+                    ++wordCounts[wordLenth];
+                } else {
+                    ++overflow;
+                }
+            }
+            wordLenth = 0;
+            state = OUT;
+        } else if (state == OUT) {
+            /* beginning of a new word */
+            state = IN;					
+            wordLenth = 1;
+        } else {
+            /* inside a word */
+            ++wordLenth;    
+        }
+    }
 
     /* find max value */
-	for (i = 0; i < MAXWORD; i++) {
+    for (i = 0; i < MAXWORD; i++) {
         if(wordCounts[i] > maxValue)
             maxValue = wordCounts[i];
     }
 
-	/* output */
-	for (i = MAXHISTOGRAM; i > 0; i--) {
+    /* output */
+    for (i = MAXHISTOGRAM; i > 0; i--) {
         for (j = 1; j < MAXWORD; j++) {
             //printf("%d", wordCounts[j]);
             if (wordCounts[j] > 0) {
@@ -68,20 +68,20 @@ main()
             }
         }
         putchar('\n');
-	}
-	for (i = 1; i < MAXWORD; i++) {
+    }
+    for (i = 1; i < MAXWORD; i++) {
         printf("%s", "___");
     }
     putchar('\n');
-	for (i = 1; i < MAXWORD; i++) {
+    for (i = 1; i < MAXWORD; i++) {
         printf("%3d", wordCounts[i]);
     }
     putchar('\n');
-	for (i = 1; i < MAXWORD; i++) {
+    for (i = 1; i < MAXWORD; i++) {
         printf("%s", "---");
     }
     putchar('\n');
-	for (i = 1; i < MAXWORD; i++) {
+    for (i = 1; i < MAXWORD; i++) {
         printf("%3d", i);
     }
     putchar('\n');

56bc67d5d68af872dad115bd034dc45979654ff5
sabonis <sabonis.tw@gmail.com>
2013-11-13 17:50:19 +0800

    Exercise 1-16 completed

---
+++ exercises/1-16.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getline2(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+    int len;
+    int max = 0;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    while ((len = getline2(line, MAXLINE)) != 0) {
+        if (len > max) {
+            copy(line, longest);
+            max = len;
+        }
+    }
+    printf("\n The number of longest line is %d\n", max);
+}
+
+int getline2(char line[], int maxline) {
+    int i, c;
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+        line[i] = c;
+    }
+    if(c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+
+    return i;
+}
+void copy(char to[], char from[]) {
+    int i;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}

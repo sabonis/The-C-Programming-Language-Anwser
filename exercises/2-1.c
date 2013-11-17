8ae125d3f0b6bc8eaba183c94ea0c30f4fde7330
sabonis <sabonis.tw@gmail.com>
2013-11-17 18:40:22 +0800

    2-1, 2-2, 2-3 completed

---
+++ exercises/2-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    char a;
+
+    printf("Singed char is range to %d\n", SCHAR_MAX);
+    printf("Unsinged char is range to %u\n", UCHAR_MAX);
+
+    printf("short min = %d\n", SHRT_MIN);
+    printf("short max = %d\n", SHRT_MAX);
+    printf("int max = %d\n", INT_MAX);
+    printf("long min = %ld\n", LONG_MIN);
+    printf("long max = %ld\n", LONG_MAX);
+
+
+
+}
